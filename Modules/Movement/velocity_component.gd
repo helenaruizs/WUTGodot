6e856482fefccbda39372d66c78b7268f15e7d52
helenaruizs <153121487+helenaIFG@users.noreply.github.com>
2024-07-22 13:52:31 -0300

    it's coyote time!

--- Modules/Movement/velocity_component.gd
+++ Modules/Movement/velocity_component.gd
@@ -1,7 +1,7 @@
 class_name VELOCITY
 extends Node
 
-@onready var timer = $Timer
+@onready var coyote_timer = $CoyoteTimer
 
 @export_group("Base Variables")
 @export var SPEED : float = 100.0		## Actor's base speed.
@@ -12,8 +12,9 @@ extends Node
 @export var jump_height_force : float = 0.4
 
 ## Coyote time WIP
-@export var coyote_time : float = 0.0
-var coyote_jump = false
+@export var coyote_time : float = 0.05
+
+var jump_available : bool = true
 
 @export_group("Node Links")
 @export var actor : CharacterBody2D 	## Parent node. 
@@ -30,31 +31,39 @@ var jump_lerp = 0.8
 ## Tracking Variables
 var prevVelocity: Vector2 = Vector2.ZERO
 
-#/
+## Add the gravity and air resistance
+func applyGravity(delta) -> void:
+		actor.velocity.y += gravity * delta
+		actor.velocity.x = lerp(prevVelocity.x, actor.velocity.x, air_resistance)	## Smooth the horizontal movement of the air state
+		actor.velocity.y = lerp(prevVelocity.y, actor.velocity.y, jump_lerp)	## Smooth the vertical movement of the air state
+
+## Coyote Timer
+func coyote_timeout():
+	jump_available = false
+
 ## Handle any velocity calculations. 
 func handleVelocity(delta):
 	prevVelocity = actor.velocity
-	## Add the gravity and air resistance
+	
+	
+	## Calculate coyote time
 	if not actor.is_on_floor():
-		if coyote_time != 0:
-			coyote_jump = true
-			timer.wait_time = coyote_time
-			timer.start()
-			if _on_timer_timeout():
-				coyote_jump = false
-		if coyote_jump == false:
-			actor.velocity.y += gravity * delta
-			actor.velocity.x = lerp(prevVelocity.x, actor.velocity.x, air_resistance)	## Smooth the horizontal movement of the air state
-			actor.velocity.y = lerp(prevVelocity.y, actor.velocity.y, jump_lerp)	## Smooth the vertical movement of the air state
-
-
+		if jump_available:
+			if coyote_timer.is_stopped():
+				coyote_timer.start(coyote_time)
+		applyGravity(delta)
+	else:
+		jump_available = true
+		coyote_timer.stop()
+	
 	## Handle jump.
-	if input_node.getJumpInputIn() and actor.is_on_floor():
+	if input_node.getJumpInputIn() and jump_available:
 		if !supress_stamina_cost:
 			if actor.stamina_node.hasEnoughStamina():
 				actor.velocity.y = JUMP_VELOCITY
 		else:
 			actor.velocity.y = JUMP_VELOCITY
+		jump_available = false
 	
 	## Jump Height
 	if input_node.getJumpInputOut() && actor.velocity.y < 0:
@@ -92,6 +101,3 @@ func calculateSpeed(addModifiers : Array[float], multModifiers : Array[float]):
 	## Multiply last (to make sure things like slows work properly) ##
 	for modifier in multModifiers:
 		currentSpeed *= modifier
-
-func _on_timer_timeout():
-	pass
